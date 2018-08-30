6e1c6adeb0a48741d60589d11cd5b42815472b08
ALguojian <3324883037@qq.com>
2018-08-30 14:01:54 +0800

    ..

---
+++ app/src/main/java/com/alguojian/example/MainActivity.java
@@ -0,0 +1,20 @@
+package com.alguojian.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * ${Descript}
+ *
+ * @author alguojian
+ * @date 2018/8/30
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
